9a3149e2b7c8d36a806fea478bfaa97dfdfb53e1
Doyun <materokatti@gmail.com>
2024-07-31 12:37:20 +0900

    node package setting

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+type SubscriptionType = 'monthly' | 'yearly';
+type DateInput = 'start' | 'end';
+
+export function calculateSubscriptionPeriod(
+  type: SubscriptionType,
+  dateType: DateInput,
+  date: Date,
+): { startDate: any; endDate: any } {
+
+
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    throw new Error('Invalid date input');
+  }
+
+  if (type === 'yearly') {
+    const startDate = new Date(date);
+    const endDate = new Date(date);
+
+    if (dateType === 'start') {
+      endDate.setFullYear(endDate.getFullYear() + 1);
+    } else {
+      startDate.setFullYear(startDate.getFullYear() - 1);
+    }
+
+    return { startDate, endDate };
+  } else if (type === 'monthly') {
+    const startDate = new Date(date);
+    const endDate = new Date(date);
+
+    if (dateType === 'start') {
+      endDate.setMonth(endDate.getMonth() + 1);
+      if (endDate.getDate() !== startDate.getDate()) {
+        endDate.setDate(0);
+      }
+    } else {
+      startDate.setMonth(startDate.getMonth() - 1);
+      console.log('startDate.getDate', startDate.getDate());
+      console.log('date.getDate', date.getDate());
+      if (startDate.getDate() !== date.getDate() && startDate.getDate() < date.getDate()) {
+        startDate.setDate(startDate.getDate() - 1);
+      }
+    }
+
+    return { startDate, endDate };
+  } else {
+    throw new Error('Invalid subscription type');
+  }
+}
